5941215822b9134b820ab26c3cf099ff69cc246b
jglee <jglee@juvis.co.kr>
2019-09-02 18:04:35 +0900

    Initial Commit

---
+++ HealthKitStepsCount/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthKitStepsCount
+//
+//  Created by 이지건 on 02/09/2019.
+//  Copyright © 2019 이지건. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
